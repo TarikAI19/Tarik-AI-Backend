c2dd624ec25c6443d412af6cbf2ee613900b0e13
raj-da <rajafgudeta@gmail.com>
2026-07-25 15:13:00 +0300

    requirement file

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/tarik_ai"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 
